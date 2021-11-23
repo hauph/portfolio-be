f96b4b4d4fff52174652f87e7273f7e25116070a
Hau Pham <hauph@hotmail.com>
2021-11-23 14:11:07 +0700

    1st commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5a1dcfe6bd0827ef2ab852826b81497f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ea9c07e6-9262-4fc6-b1ed-0c5c1ec62680'
+};
