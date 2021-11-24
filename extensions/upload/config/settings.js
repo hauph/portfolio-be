7e8c08499e2e48ce6d718e5a17c3aba554e10acf
Hau Pham <hauph@hotmail.com>
2021-11-24 10:11:43 +0700

    revert settings.js

--- extensions/upload/config/settings.js
+++ extensions/upload/config/settings.js
@@ -1,9 +1,9 @@
-module.exports = ({env}) => ({
+module.exports = {
   provider: "do",
   providerOptions: {
-    key: env('DO_SPACE_ACCESS_KEY'),
-    secret: env('DO_SPACE_SECRET_KEY'),
-    endpoint: env('DO_SPACE_ENDPOINT'),
-    space: env('DO_SPACE_BUCKET'),
+    key: process.env.DO_SPACE_ACCESS_KEY,
+    secret: process.env.DO_SPACE_SECRET_KEY,
+    endpoint: process.env.DO_SPACE_ENDPOINT,
+    space: process.env.DO_SPACE_BUCKET,
   },
-});
+};
