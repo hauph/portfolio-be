d72b6149282c53ec493307d7898069471c93ae3f
Hau Pham <hauph@hotmail.com>
2021-11-24 09:07:45 +0700

    update settings

--- extensions/upload/config/settings.js
+++ extensions/upload/config/settings.js
@@ -1,9 +1,9 @@
-module.exports = {
+module.exports = ({env}) => ({
   provider: "do",
   providerOptions: {
-    key: process.env.DO_SPACE_ACCESS_KEY,
-    secret: process.env.DO_SPACE_SECRET_KEY,
-    endpoint: 'sgp1.digitaloceanspaces.com',
-    space: process.env.DO_SPACE_BUCKET,
+    key: env('DO_SPACE_ACCESS_KEY'),
+    secret: env('DO_SPACE_SECRET_KEY'),
+    endpoint: env('DO_SPACE_ENDPOINT'),
+    space: env('DO_SPACE_BUCKET'),
   },
-};
+});
